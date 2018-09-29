7eff6861861ec2e37ca0ac5c074a56a052b84865
CL <chaelin.kim@kaist.ac.kr>
2018-09-29 14:30:37 +0900

    seperate cpp to cpp and h

--- HW2/HW2_ChaelinKim/SmartModeling_KCL/SmartModeling_KCL/SmartModeling_CL.cpp
+++ HW2/HW2_ChaelinKim/SmartModeling_KCL/SmartModeling_KCL/SmartModeling_CL.cpp
@@ -1,44 +1,16 @@
-#include <maya/MPxCommand.h>
+#include "SmartModeling_CL.h"
 #include <maya/MGlobal.h>
 #include <maya/MFnPlugin.h>
-#include <maya/MSelectionList.h>
-#include <maya/MItSelectionList.h>
 #include <maya/MPoint.h>
 #include <maya/MArgList.h>
-#include <maya/MSyntax.h>
-
 #include <maya/MArgDatabase.h>
 #include <maya/MDagPath.h>
-#include <maya/MDagPathArray.h>
-#include <maya/MObjectArray.h>
 #include <maya/MFnMesh.h>
 #include <maya/MFnTransform.h>
 #include <maya/MItMeshVertex.h>
 #include <maya/MFnSet.h>
-#include <maya/MDGModifier.h>
 #include <maya/MString.h>
-#include <maya/MStringArray.h>
-#include <string.h>
 #include <math.h>
-
-class SmartModeling : public MPxCommand {
-public:
-	virtual MStatus doIt(const MArgList&);
-	virtual MStatus undoIt();
-	virtual MStatus redoIt();
-	virtual bool isUndoable() const { return true;  };
-
-	static void *creator() { return new SmartModeling; }
-	static MSyntax newSyntax();
-
-private:
-	MObjectArray instanceArray;
-	MDGModifier dgMod;
-	int height;
-	MSelectionList selectPlane;
-	MSelectionList selectObjects;
-	MStringArray objectArr;
-};
 
 const char *heightFlag = "-h", *heightLongFlag = "-height";
 const char *objectFlag = "-o", *objectLongFlag = "-objects";
@@ -55,11 +27,8 @@ MSyntax SmartModeling::newSyntax() {
 }
 
 MStatus SmartModeling::doIt(const MArgList &args) {
-	//MGlobal::displayInfo("working?");
-
 	MString plane;
 	MString objects;
-	//MStringArray objectArr;
 
 	MStatus stat;
 	MArgDatabase argData(syntax(), args, &stat);
@@ -67,6 +36,7 @@ MStatus SmartModeling::doIt(const MArgList &args) {
 	
 	if (argData.isFlagSet(objectFlag))
 		argData.getFlagArgument(objectFlag, 0, objects);
+	objects.split('|', objectArr);
 
 	if (argData.isFlagSet(planeFlag))
 		argData.getFlagArgument(planeFlag, 0, plane);
@@ -74,73 +44,72 @@ MStatus SmartModeling::doIt(const MArgList &args) {
 	if (argData.isFlagSet(heightFlag))
 		argData.getFlagArgument(heightFlag, 0, height);
 
-	MGlobal::displayInfo(objects);
-	objects.split('|', objectArr);	
-
-	//MSelectionList selectPlane;
+	
+	// set plane
 	selectPlane.clear();
 	MGlobal::getSelectionListByName(plane, selectPlane);
 
-	//MSelectionList selectObjects;
 	selectObjects.clear();
 	for (int i = 0; i < objectArr.length(); i++) {
 		selectObjects.add(objectArr[i]);
 	}
 
-	//// Register Plane
-	//MDagPath dagMesh;
-	//MFnMesh fnMesh;
-	//fnMesh.setObject(dagMesh);
+	/* Go to redo function
+	// Register Plane
+	MDagPath dagMesh;
+	MFnMesh fnMesh;
+	fnMesh.setObject(dagMesh);
 
-	//selectPlane.getDagPath(0, dagMesh);
+	selectPlane.getDagPath(0, dagMesh);
 
-	//// Iterate over the mesh vertices
-	//int vertIndex;
-	//MItMeshVertex iterVtx(dagMesh);
+	// Iterate over the mesh vertices
+	int vertIndex;
+	MItMeshVertex iterVtx(dagMesh);
 
-	//// Initial Shading Group
-	//// Refer to a Ground-Shadow plug-in in your textbook
-	//MSelectionList sList;
-	//MObject shadingGroupObj;
+	// Initial Shading Group
+	// Refer to a Ground-Shadow plug-in in your textbook
+	MSelectionList sList;
+	MObject shadingGroupObj;
 
-	//sList.clear();
-	//MGlobal::getSelectionListByName("initialShadingGroup", sList);
-	//sList.getDependNode(0, shadingGroupObj);
+	sList.clear();
+	MGlobal::getSelectionListByName("initialShadingGroup", sList);
+	sList.getDependNode(0, shadingGroupObj);
 
-	//MFnSet shadingGroupFn;
-	//shadingGroupFn.setObject(shadingGroupObj);
+	MFnSet shadingGroupFn;
+	shadingGroupFn.setObject(shadingGroupObj);
 
-	//// Clear MObjectArray
-	//instanceArray.clear();
+	// Clear MObjectArray
+	instanceArray.clear();
 
-	//for (; !iterVtx.isDone(); iterVtx.next()) {
-	//	MPoint pt = iterVtx.position(MSpace::kWorld);
-	//	vertIndex = iterVtx.index();
+	for (; !iterVtx.isDone(); iterVtx.next()) {
+		MPoint pt = iterVtx.position(MSpace::kWorld);
+		vertIndex = iterVtx.index();
 
-	//	int randomModelNum = (rand() % (objectArr.length()));
+		int randomModelNum = (rand() % (objectArr.length()));
 
-	//	// Register objects
-	//	MObject dagModel;
-	//	MFnTransform fnTransform;
-	//	selectObjects.getDependNode(randomModelNum, dagModel);
-	//	fnTransform.setObject(dagModel);
-	//	MObject instance = fnTransform.duplicate();
+		// Register objects
+		MObject dagModel;
+		MFnTransform fnTransform;
+		selectObjects.getDependNode(randomModelNum, dagModel);
+		fnTransform.setObject(dagModel);
+		MObject instance = fnTransform.duplicate();
 
-	//	// Register the duplicated instance and set its transformation
-	//	MFnTransform fnInstance(instance);
-	//	fnInstance.setTranslation(pt, MSpace::kTransform);
+		// Register the duplicated instance and set its transformation
+		MFnTransform fnInstance(instance);
+		fnInstance.setTranslation(pt, MSpace::kTransform);
 
-	//	instanceArray.append(instance);
+		instanceArray.append(instance);
 
-	//	double scale[3];
-	//	fnInstance.getScale(scale);
-	//	scale[1] = rand() % height + 1;
-	//	fnInstance.setScale(scale);
+		double scale[3];
+		fnInstance.getScale(scale);
+		scale[1] = rand() % height + 1;
+		fnInstance.setScale(scale);
 
 
-	//	// Connect a duplicated instance with initial shading group during the iteration
-	//	shadingGroupFn.addMember(instance);
-	//}
+		// Connect a duplicated instance with initial shading group during the iteration
+		shadingGroupFn.addMember(instance);
+	}
+	*/
 
 	return redoIt();
 }
@@ -162,17 +131,6 @@ MStatus SmartModeling::undoIt() {
 }
 
 MStatus SmartModeling::redoIt() {
-	MGlobal::displayInfo("redo");
-	/*MString selectedObjects;
-
-	for (int i = 0; i < instanceArray.length(); i++) {
-		MFnDagNode tempNode;
-		tempNode.setObject(instanceArray[i]);
-		selectedObjects += tempNode.name() + " ";
-	}
-
-	MGlobal::executeCommand(MString("select -r ") + selectedObjects + " ;");*/
-
 	// Register Plane
 	MDagPath dagMesh;
 	MFnMesh fnMesh;
---
+++ HW2/HW2_ChaelinKim/SmartModeling_KCL/SmartModeling_KCL/SmartModeling_CL.h
@@ -0,0 +1,32 @@
+#ifndef SMARTMODELING_KCL_H
+#define SMARTMODELING_KCL_H
+
+#include <maya/MPxCommand.h>
+#include <maya/MSyntax.h>
+
+#include <maya/MObjectArray.h>
+#include <maya/MSelectionList.h>
+#include <maya/MStringArray.h>
+#include <maya/MDGModifier.h>
+
+class SmartModeling : public MPxCommand {
+public:
+	virtual MStatus doIt(const MArgList&);
+	virtual MStatus undoIt();
+	virtual MStatus redoIt();
+	virtual bool isUndoable() const { return true; };
+
+	static void *creator() { return new SmartModeling; }
+	static MSyntax newSyntax();
+
+private:
+	MObjectArray instanceArray;
+	MSelectionList selectPlane;
+	MSelectionList selectObjects;
+	MStringArray objectArr;
+
+	int height;
+
+	MDGModifier dgMod;
+};
+#endif // !SMARTMODELING_KCL_H
