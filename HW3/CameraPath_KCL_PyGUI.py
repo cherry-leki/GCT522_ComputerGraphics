da0ecf2e4aa361e96536a6b9f84c71ddf9bb2b6c
CL <chaelin.kim@kaist.ac.kr>
2018-10-04 19:06:36 +0900

    add placeCamera function

--- HW3/CameraPath_KCL_PyGUI.py
+++ HW3/CameraPath_KCL_PyGUI.py
@@ -2,6 +2,8 @@ import maya.cmds as cmds
 import sys
 import os
 
+list_cameraPlace = ['Right', 'Left', 'Front', 'Back']
+
 # Add target object
 def addTargetObj(*args):
     selectedTargetObj = cmds.ls(selection=True)
@@ -13,11 +15,11 @@ def addTargetObj(*args):
         return
     cmds.textScrollList(targetObj, edit=True, ra=True)
     cmds.textScrollList(targetObj, edit=True, append=selectedTargetObj)
-    
+
 # Remove target object
 def removeTargetObj(*args):
     cmds.textScrollList(targetObj, edit=True, ra=True)
-        
+
 # Add Curve object
 def addCurve(*args):
     selectedCurve = cmds.ls(selection=True)
@@ -29,14 +31,36 @@ def addCurve(*args):
         return
     cmds.textScrollList(targetCurve, edit=True, ra=True)
     cmds.textScrollList(targetCurve, edit=True, append=selectedCurve)
-        
+
 # Remove Curve object
 def removeCurve(*args):
     cmds.textScrollList(targetCurve, edit=True, ra=True)
 
 # Place a camera in accordance with direction and distance
 def placeCamera(*args):
-    print "Place Camera"
+    cameraDist = cmds.textField(distance, q=True, tx=True)
+    if not cameraDist:
+        print "Please input the distance"
+        return
+
+    target = cmds.textScrollList(targetObj, query=True, ai=True)
+    target = ''.join(target)
+    # Target Position
+    targetPos = cmds.getAttr(target + '.translate')
+    cameraPos = list(targetPos[0])
+    cameraPos[2] += float(cameraDist)
+
+    targetRot = cmds.getAttr(target + '.rotate')
+    cameraRot = list(targetRot[0])
+
+    cameraDirSwitch = { list_cameraPlace[0]: 90, list_cameraPlace[1]: -90,
+                    list_cameraPlace[2]: 0, list_cameraPlace[3]: 180}
+    cameraDir = cameraDirSwitch.get(cmds.optionMenu(cameraPlace, q=True, v=True), 0)
+    cameraRot[1] += float(cameraDir)
+
+    newCamera = cmds.camera()
+    cmds.setAttr(newCamera[0] + '.translate', cameraPos[0], cameraPos[1], cameraPos[2], type="double3")
+    cmds.setAttr(newCamera[0] + '.rotate', cameraRot[0], cameraRot[1], cameraRot[2], type="double3")
 
 # Make the camera follow the curve path with keyframes and interval length
 def followPath(*args):
@@ -70,7 +94,6 @@ cmds.button(label="Add", p="curveLayout", w=40, command=addCurve)
 cmds.button(label="Remove", p="curveLayout", w=55, command=removeCurve)
 
 ''' Camera Placement Layout '''
-list_cameraPlace = ['Right', 'Left', 'Front', 'Back']
 cmds.rowLayout("cameraPlaceLayout", p="mainCol", numberOfColumns = 7)
 cmds.text(label="Camera Placement", p="cameraPlaceLayout", w=103)
 cameraPlace = cmds.optionMenu("cameraPlaceItem", p="cameraPlaceLayout", w=97)
@@ -80,7 +103,7 @@ cmds.text("", w=7)
 cmds.text(label="Distance", p="cameraPlaceLayout", w=50)
 distance = cmds.textField("distanceTextField", p="cameraPlaceLayout", w=117)
 cmds.text(" ", w=5)
-cmds.button(label="Place Camera", p="cameraPlaceLayout", w=97)
+cmds.button(label="Place Camera", p="cameraPlaceLayout", w=97, command=placeCamera)
 
 ''' Keyframes Layout '''
 cmds.rowLayout("keyframeLayout", p="mainCol", numberOfColumns = 5)
