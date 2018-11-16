531c4bccfd413157671aefd2205eda0c228a0a74
CL <chaelin.kim@kaist.ac.kr>
2018-11-16 17:44:31 +0900

    complete process 1

--- HW5/toonShader_CL/toonShader_CL/toonShaderOverride.h
+++ HW5/toonShader_CL/toonShader_CL/toonShaderOverride.h
@@ -1,6 +1,23 @@
 #ifndef _toonShaderOverride
 #define _toonShaderOverride
 
+//-
+// ===========================================================================
+// Copyright 2012 Autodesk, Inc. All rights reserved.
+//
+// Use of this software is subject to the terms of the Autodesk license
+// agreement provided at the time of installation or download, or which
+// otherwise accompanies this software in either electronic or hard copy form.
+// ===========================================================================
+//+
+
+// Example Plugin: lambertShader.cpp
+//
+// This is the MPxSurfaceShadingNodeOverride implementation to go along with
+// the node defined in lambertShader.cpp. This provides draw support in
+// Viewport 2.0.
+//
+
 #include <maya/MPxSurfaceShadingNodeOverride.h>
 
 class toonShaderOverride : public MHWRender::MPxSurfaceShadingNodeOverride {
